624b0df932238b118370d9f7fcc58670443c5ade
r-salogubov <r.salogubov@seraphim.online>
2019-11-27 18:04:05 +0300

    wip

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 // Created by rsalogub on 27.11.19.
 //
 
+#include "window.h"
+
 int main()
 {
+    FRWindows::Window(800, 600, "window");
     return 0;
 }
---
+++ window.h
@@ -0,0 +1,12 @@
+//
+// Created by rsalogub on 27.11.19.
+//
+
+#ifndef WINDOW_H
+#define WINDOW_H
+#include "realization/FRX11WindowsManager/window.h"
+namespace FRWindows
+{
+    using FRX11Windows::Window;
+}
+#endif //WINDOW_H
